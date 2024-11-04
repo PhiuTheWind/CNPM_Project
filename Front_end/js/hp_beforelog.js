a2c092be30363a6ebafaec6b16d848529eeff3fb
Hoàng Văn Phi <125084363+PhiuTheWind@users.noreply.github.com>
2024-11-04 23:49:05 +0700

    Add files via upload

---
+++ Front_end/js/hp_beforelog.js
@@ -0,0 +1,9 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     alert("Welcome to my website!");
+// });
+
+// const form = document.querySelector("form");
+// form.addEventListener("submit", function(event) {
+//     event.preventDefault();
+//     alert("Thank you for submitting the form!");
+// });
