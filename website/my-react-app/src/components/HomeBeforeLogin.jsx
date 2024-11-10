503141151ceaf672932c3a70220fb16a8110f91b
Olwenyu <van.huynhcsolwenyu@hcmut.edu.vn>
2024-11-10 22:17:18 +0700

    Modify css to css module

--- website/my-react-app/src/components/HomeBeforeLogin.jsx
+++ website/my-react-app/src/components/HomeBeforeLogin.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/HomeBeforeLogin.css';
+import styles from '../styles/HomeBeforeLogin.module.css';
 import spss from '../assets/PRINTING SERVICE.png';
 import ava from '../assets/avatar.png';
 import banner from '../assets/banner.png';
@@ -8,23 +8,23 @@ import login from '../assets/login.png';
 import Footer from './Footer';
 function HomeBeforeLogin(){
     return(
-        <div className="container">
+        <div className={styles.container}>
             <header>
-                <div className="header-logo">
+                <div className={styles.header_logo}>
                     <img src={spss} alt="BK Student Smart Printing Service" />
                 </div>
-                <div className="header-right">
-                    <div className="avatar-container">
-                        <img src={ava} className="avatar" alt="Avatar" />
-                        <p className="welcome-button">XIN CHÀO</p>
+                <div className={styles.header_right}>
+                    <div className={styles.avatar_container}>
+                        <img src={ava} className={styles.avatar} alt="Avatar" />
+                        <p className={styles.welcome_button}>XIN CHÀO</p>
                     </div>
                 </div>
             </header>
 
-            <section className="banner">
-                <img src={banner} alt="Banner" className="banner-image" />
+            <section className={styles.banner}>
+                <img src={banner} alt="Banner" className={styles.banner_image} />
                 <Link to='/user'>
-                    <img src={login} alt="Login button" className="login-image"></img>                
+                    <img src={login} alt="Login button" className={styles.login_image}></img>                
                 </Link>
             </section>
 
