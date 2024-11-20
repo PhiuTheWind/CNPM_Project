983c8f36024ac8fb3a1b62b2b0a51e66bd5c61eb
Olwenyu <165460360+Olwenyu@users.noreply.github.com>
2024-11-20 11:16:44 +0700

    Modify file structure

--- Front_end/js/hp_beforelog.js
+++
@@ -1,9 +0,0 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     alert("Welcome to my website!");
-// });
-
-// const form = document.querySelector("form");
-// form.addEventListener("submit", function(event) {
-//     event.preventDefault();
-//     alert("Thank you for submitting the form!");
-// });
