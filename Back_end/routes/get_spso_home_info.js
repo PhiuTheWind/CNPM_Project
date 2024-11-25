582a5f0784e87742297b1ea742189e659cda90a2
kansuu <nguyeenkhanhs.cs@gmail.com>
2024-11-25 20:24:49 +0700

    modifiy spso route

--- Back_end/routes/get_spso_home_info.js
+++ Back_end/routes/get_spso_home_info.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const spsoController = require('../controllers/spsoController');
 
-// route.post('/add_printer', spsoController.add_printer);
-// route.post('/modify_status', spsoController.modify_status);
-// route.get('/report', spsoController.report);
+route.post('/add_printer', spsoController.add_printer);
+route.post('/modify_status', spsoController.modify_status);
+route.get('/report', spsoController.report);
 
 module.exports = router;    
