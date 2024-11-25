3c7792076aad9f1a4e6ed9b1e6b4d65b6b5c5b68
Olwenyu <165460360+Olwenyu@users.noreply.github.com>
2024-11-25 21:31:46 +0700

    Modify db.sql

--- Back_end/routes/get_home_info.js
+++ Back_end/routes/get_home_info.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const homeController = require('../controllers/homeController');
 const authenticate = require('../middlewares/authenticate');
 
-// router.post('/login/student', homeController.loginStudent);
-// router.post('/login/spso', homeController.loginSPSO);
-// router.post('/', authenticate, homeController.getUserByUsername);
+router.post('/login/student', homeController.loginStudent);
+router.post('/login/spso', homeController.loginSPSO);
+router.post('/', authenticate, homeController.getUserByUsername);
 
 module.exports = router;
