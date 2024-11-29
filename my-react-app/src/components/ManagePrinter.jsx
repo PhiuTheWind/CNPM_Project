8e2a30c8faeaea9a850a99496c4d4b2c95642583
Olwenyu <165460360+Olwenyu@users.noreply.github.com>
2024-11-29 10:31:17 +0700

    Creating popup for setting ptrinter

--- my-react-app/src/components/ManagePrinter.jsx
+++ my-react-app/src/components/ManagePrinter.jsx
@@ -7,10 +7,17 @@ import styles from '../styles/ManagePrinter.module.css'
 import { FaNewspaper } from "react-icons/fa6"
 import { IoSearch, IoEyeSharp, IoSettingsSharp } from "react-icons/io5"
 import { TiArrowSortedDown, TiArrowSortedUp } from "react-icons/ti"
+import MOCK_DATA from '../assets/MOCK_DATA'
 
 function ManagePrinter() {
   const [searchTerm, setSearchTerm] = useState('');
+  const [isSettingOpen, setIsSettingOpen] = useState(false);
+  const [selectedPrinter, setSelectedPrinter] = useState(null);
+  const [isActive, setIsActive] = useState(false);
+  const [selectedStatus, setStatus] = useState("")
+  const statusOption = ["Bật", "Tắt", "Bảo trì"]
   const navigate = useNavigate()
+
   const COLUMNS = [
     {
       Header: 'ID MÁY IN',
@@ -38,10 +45,10 @@ function ManagePrinter() {
       Cell: ({ value }) => (
         <div
           className={`${styles.statusBadge} ${value === "Bật"
-              ? styles.bật
-              : value === "Tắt"
-                ? styles.tắt
-                : styles.bảo_trì
+            ? styles.bật
+            : value === "Tắt"
+              ? styles.tắt
+              : styles.bảo_trì
             }`}
         >
           {value}
@@ -51,46 +58,27 @@ function ManagePrinter() {
     {
       Header: 'LỊCH SỬ IN',
       accessor: 'log',
-      Cell: ({ row }) => <button 
-        className={styles.button} 
+      Cell: ({ row }) => <button
+        className={styles.button}
         onClick={() => {
-        console.log('Row Data:', row.original); // Log the entire row data
-      }}>
-        <IoEyeSharp className={styles.icon}/>
+          console.log('Row Data:', row.original); // Log the entire row data
+        }}>
+        <IoEyeSharp className={styles.icon} />
       </button>,
     },
     {
       Header: 'CÀI ĐẶT',
       accessor: 'setting',
-      Cell: ({ row }) => <button 
+      Cell: ({ row }) => <button
         className={styles.button}
         onClick={() => {
-          console.log('Row Data:', row.original); // Log the entire row data
-      }}>
-        <IoSettingsSharp className={styles.icon}/>
+          setSelectedPrinter(row.original)
+          setIsSettingOpen(true)
+        }}>
+        <IoSettingsSharp className={styles.icon} />
       </button>,
     },
   ]
-  const MOCK_DATA = [{"id":"Printer#1","location":"402A4-CS1","paper":93,"status":"Bảo trì"},
-    {"id":"Printer#2","location":"401A4-CS1","paper":80,"status":"Bật"},
-    {"id":"Printer#3","location":"402A4-CS1","paper":407,"status":"Tắt"},
-    {"id":"Printer#4","location":"401A4-CS1","paper":194,"status":"Bảo trì"},
-    {"id":"Printer#5","location":"401A4-CS1","paper":333,"status":"Bảo trì"},
-    {"id":"Printer#6","location":"402A4-CS1","paper":483,"status":"Bảo trì"},
-    {"id":"Printer#7","location":"401A4-CS1","paper":155,"status":"Bật"},
-    {"id":"Printer#8","location":"402A4-CS1","paper":183,"status":"Bật"},
-    {"id":"Printer#9","location":"401A4-CS1","paper":343,"status":"Bật"},
-    {"id":"Printer#10","location":"401A4-CS1","paper":142,"status":"Bật"},
-    {"id":"Printer#11","location":"401A4-CS1","paper":347,"status":"Bật"},
-    {"id":"Printer#12","location":"402A4-CS1","paper":276,"status":"Bật"},
-    {"id":"Printer#13","location":"401A4-CS1","paper":287,"status":"Bật"},
-    {"id":"Printer#14","location":"402A4-CS1","paper":214,"status":"Bật"},
-    {"id":"Printer#15","location":"401A4-CS1","paper":139,"status":"Tắt"},
-    {"id":"Printer#16","location":"401A4-CS1","paper":322,"status":"Bật"},
-    {"id":"Printer#17","location":"402A4-CS1","paper":329,"status":"Bật"},
-    {"id":"Printer#18","location":"401A4-CS1","paper":490,"status":"Tắt"},
-    {"id":"Printer#19","location":"401A4-CS1","paper":13,"status":"Bảo trì"},
-    {"id":"Printer#20","location":"401A4-CS1","paper":149,"status":"Bật"}]
 
   const columns = useMemo(() => COLUMNS, [])
   const data = useMemo(() => MOCK_DATA, [])
@@ -106,30 +94,30 @@ function ManagePrinter() {
     headerGroups,
     rows,
     prepareRow
-  } = tableInstance 
+  } = tableInstance
 
   return (
     <div className={styles.container}>
       <Header text='SPSO NAME' showLogout={true} />
       <div className={styles.search_add}>
-          <div className={styles.search_wrapper}>
-            <IoSearch className={styles.search_icon}/>
-            <input
-              type="text"
-              className={styles.searchInput}
-              placeholder="Nhập ID máy in"
-              value={searchTerm}
-              onChange={(e) => {
-                setSearchTerm(e.target.value);
-              }}
-            />
-          </div>
-         
-          <button className={styles.addPrinter} onClick={() => navigate('/spso_homepage/manage_printer/add_printer')}>
-            THÊM MÁY IN
-          </button>
+        <div className={styles.search_wrapper}>
+          <IoSearch className={styles.search_icon} />
+          <input
+            type="text"
+            className={styles.searchInput}
+            placeholder="Nhập ID máy in"
+            value={searchTerm}
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+            }}
+          />
         </div>
-      
+
+        <button className={styles.addPrinter} onClick={() => navigate('/spso_homepage/manage_printer/add_printer')}>
+          THÊM MÁY IN
+        </button>
+      </div>
+
 
       <div className={styles.table_printer}>
         <div className={styles.table_wrapper}>
@@ -141,7 +129,7 @@ function ManagePrinter() {
                     <th className={styles.th} {...column.getHeaderProps(column.getSortByToggleProps())}>
                       {column.render("Header")}
                       <span>
-                        {column.isSorted ? (column.isSortedDesc ? <TiArrowSortedDown/> : <TiArrowSortedUp/>) : ''}
+                        {column.isSorted ? (column.isSortedDesc ? <TiArrowSortedDown /> : <TiArrowSortedUp />) : ''}
                       </span>
                     </th>
                   ))}
@@ -166,6 +154,87 @@ function ManagePrinter() {
         </div>
       </div>
 
+      {isSettingOpen && (
+        <div className={styles.popup}>
+          <div className={styles.popup_info}>
+            <h2 className={styles.h2}>Thông Tin Máy In</h2>
+            <div className={styles.info}>
+              <div className={styles.row}>
+                <label className={styles.field}>ID Máy In:</label>
+                <span className={styles.value}>{selectedPrinter?.id}</span>
+              </div>
+
+              <div className={styles.row}>
+                <label className={styles.field}>Tên Máy In:</label>
+                <span className={styles.value}></span>
+              </div>
+
+              <div className={styles.row}>
+                <label className={styles.field}>Địa Chỉ IP:</label>
+                <span className={styles.value}></span>
+              </div>
+
+              <div className={styles.row}>
+                <label className={styles.field}>Vị Trí:</label>
+                <span className={styles.value}>{selectedPrinter?.location}</span>
+              </div>
+
+              <div className={styles.row}>
+                <label className={styles.field}>Số Giấy:</label>
+                <span className={styles.value}>{selectedPrinter?.paper}</span>
+              </div>
+
+              <div className={styles.row}>
+                <label className={styles.field}>Trạng Thái:</label>
+                <span className={styles.value}>{selectedPrinter?.status}</span>
+              </div>
+            </div>
+
+            <div className={styles.mod}>
+              <div className={styles.row}>
+                <label className={styles.field}>Điều chỉnh trạng thái:</label>
+                <div className={styles.dropdown}>
+                  <div className={styles.dropdown_btn} onClick={(e) => setIsActive(!isActive)}>
+                    <p>
+                      {selectedPrinter?.status}
+                    </p>
+                  </div>
+
+                  {isActive && (
+                    <div className={styles.dropdown_content}>
+                      {statusOption.map(option => (
+                        <div key={option} className={styles.dropdown_item}
+                          onClick={() => {
+                            setStatus(option)
+                            setIsActive(!isActive)
+                          }
+                          }>
+                          {option}
+                        </div>
+                      ))}
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.popup_btn}>
+              <button
+                className={styles.close_popup}
+                onClick={() => setIsSettingOpen(false)}
+              >
+                HỦY
+              </button>
+              <button
+                className={styles.save_popup}
+                onClick={() => setIsSettingOpen(false)}
+              >
+                LƯU
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
 
       <Footer />
     </div>
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -24,7 +24,7 @@ import App from './App'; // Main App component
 // Create a root element and render the App component
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <App />,
-  {/* </React.StrictMode> */}
+  </React.StrictMode>
 );
