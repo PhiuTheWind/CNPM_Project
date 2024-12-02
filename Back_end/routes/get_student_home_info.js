75b582b4099634983da0ce26729e6c2a4c6d149d
Khang <khangnha7@gmail.com>
2024-12-02 12:15:05 +0700

    them api get page balance

--- Back_end/routes/get_student_home_info.js
+++ Back_end/routes/get_student_home_info.js
@@ -5,5 +5,7 @@ const studentController = require('../controllers/studentController');
 router.get('/maintenance', studentController.maintenance);
 router.get('/guideline', studentController.guideline);
 router.get('/contact', studentController.contact);
+router.get('/chooseprinter', studentController.get_printer_list_for_student);
+router.post('/pagebalance', studentController.get_pagebalance);
 
 module.exports = router;
